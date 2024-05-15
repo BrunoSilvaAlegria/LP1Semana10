aacaff02f5795199bc34bb0692270981652cf222
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-05-15 22:10:25 +0100

    Ex 1, transform PlayerManager4 code to MVC format

---
+++ PlayerManagerMVC/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayerManagerMVC
+{
+    public class Program
+    {
+        private static void Main() //Model created
+        {
+            // Initialize the player list with two players using collection
+            // initialization syntax
+                List<Player> playerList = new List<Player>()
+                {
+                new Player("Ana", 100),
+                new Player("Bruno", 500)
+                };
+
+            Controller controller = new Controller(playerList);
+
+            IView view = new UglyView(controller, playerList);
+
+            //Start the Controller instance
+            controller.Start(view);
+        }
+    }
+}
